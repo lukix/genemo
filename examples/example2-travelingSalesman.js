d060e2bab58e8a3defb6b61bdb7e9914615f33da
lukix <lukix.95@gmail.com>
2018-10-28 00:39:20 +0200

    Add orderOne crossover

--- examples/example2-travelingSalesman.js
+++ examples/example2-travelingSalesman.js
@@ -1,8 +1,6 @@
-const R = require('ramda');
 const shuffle = require('shuffle-array');
 const GMO = require('../lib');
 const distances = require('./data/distances.json');
-const randomFromRange = require('../lib/utils/randomFromRange');
 const findBestIndividual = require('../lib/utils/findBestIndividual');
 
 const cities = [...Array(distances.length).keys()];
@@ -27,36 +25,6 @@ const elitistSelection = ({
   return selectedIndividuals;
 };
 
-const createChildUsingOrderOneCrossover = ([parent1, parent2], minIndex, maxIndex) => {
-  const individualLength = parent1.length;
-  const child = new Array(individualLength).fill();
-  R.range(minIndex, maxIndex + 1).forEach((index) => {
-    child[index] = parent1[index];
-  });
-
-  let childIndex = maxIndex + 1;
-  [...R.range(maxIndex + 1, individualLength), ...R.range(0, maxIndex + 1)].forEach((index) => {
-    if (!child.includes(parent2[index])) {
-      child[childIndex % individualLength] = parent2[index];
-      childIndex += 1;
-    }
-  });
-  return child;
-};
-
-const orderOneCrossover = ([mother, father]) => {
-  const individualLength = mother.length;
-  const index1 = randomFromRange(0, individualLength - 1);
-  const index2 = randomFromRange(0, individualLength - 1);
-  const minIndex = Math.min(index1, index2);
-  const maxIndex = Math.max(index1, index2);
-
-  const son = createChildUsingOrderOneCrossover([mother, father], minIndex, maxIndex);
-  const daughter = createChildUsingOrderOneCrossover([father, mother], minIndex, maxIndex);
-
-  return [son, daughter];
-};
-
 const selection = elitistSelection({
   minimalizeFitness: true,
   nextSelection: GMO.selection.tournament({ tournamentSize: 3, minimalizeFitness: true }),
@@ -69,7 +37,7 @@ const evolutionOptions = {
   }),
   selection,
   reproduce: GMO.reproduce({
-    crossover: orderOneCrossover,
+    crossover: GMO.crossover.orderOne,
     mutate: GMO.mutation.swapTwoGenes,
     mutationProbability: 0.02,
   }),
--- src/crossover/index.js
+++ src/crossover/index.js
@@ -1,9 +1,11 @@
 const singlePoint = require('./singlePoint');
 const twoPoint = require('./twoPoint');
 const kPoint = require('./kPoint');
+const orderOne = require('./orderOne');
 
 module.exports = {
   singlePoint,
   twoPoint,
   kPoint,
+  orderOne,
 };
