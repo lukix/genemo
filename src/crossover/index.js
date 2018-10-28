fb8b70d1645d03686ddb6eb841a509d4f5b14a87
lukix <lukix.95@gmail.com>
2018-10-28 19:15:56 +0100

    Add PMX crossover

--- src/crossover/index.js
+++ src/crossover/index.js
@@ -2,10 +2,12 @@ const singlePoint = require('./singlePoint');
 const twoPoint = require('./twoPoint');
 const kPoint = require('./kPoint');
 const orderOne = require('./orderOne');
+const PMX = require('./pmx');
 
 module.exports = {
   singlePoint,
   twoPoint,
   kPoint,
   orderOne,
+  PMX,
 };
