7e15af19ebcec41925173033d2b19e147cbab809
lukix <lukix.95@gmail.com>
2018-11-03 22:29:38 +0100

    Add jest configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
   extends: 'airbnb-base',
+  env: {
+    'jest/globals': true,
+  },
+  plugins: ['jest'],
   rules: {
     'max-len': ['error', { code: 100, ignoreComments: true }],
     'func-names': ['off'],
