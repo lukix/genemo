f268c7a5c992892bda651917866c70626e009d19
lukix <lukix.95@gmail.com>
2018-11-08 22:32:57 +0100

    Add tests for binaryRangeSearch function

--- src/utils/binaryRangeSearch.js
+++ src/utils/binaryRangeSearch.js
@@ -1,20 +1,22 @@
-const binaryRangeSearch = (() => {
-  const privateFunction = (array, compare, begIndex, endIndex) => {
-    if (begIndex >= endIndex) {
-      return array[begIndex];
-    }
+const binaryRangeSearchInner = (array, condition, begIndex, endIndex) => {
+  if (begIndex >= endIndex) {
+    return array[begIndex];
+  }
 
-    const index = begIndex + Math.floor((endIndex - begIndex) / 2);
-    return compare(array[index])
-      ? privateFunction(array, compare, begIndex, index) // left
-      : privateFunction(array, compare, index + 1, endIndex); // right
-  };
+  const index = begIndex + Math.floor((endIndex - begIndex) / 2);
+  return condition(array[index])
+    ? binaryRangeSearchInner(array, condition, begIndex, index) // left
+    : binaryRangeSearchInner(array, condition, index + 1, endIndex); // right
+};
 
-  const publicFunction = (array, compare) => (
-    privateFunction(array, compare, 0, array.length - 1)
-  );
-
-  return publicFunction;
-})();
+/**
+ * Finds first element in an array which fulfills given condition.
+ * @param {Array<*>} array Array to be searched
+ * @param {(*) => boolean)} condition Informs if the algorithm should move to the left or to the right from the current element
+ */
+const binaryRangeSearch = (array, condition) => {
+  const result = binaryRangeSearchInner(array, condition, 0, array.length - 1);
+  return condition(result) ? result : undefined;
+};
 
 module.exports = binaryRangeSearch;
---
+++ test/utils/binaryRangeSearch.test.js
@@ -0,0 +1,55 @@
+const binaryRangeSearch = require('../../lib/utils/binaryRangeSearch');
+
+const inputArray = [-20, -7, 1, 3, 4, 5, 8, 9, 11, 26];
+
+describe('binaryRangeSearch', () => {
+  test('Finds element when it is first in the array', () => {
+    const condition = n => n >= -30;
+
+    // Expected result
+    const expectedResult = -20;
+
+    const result = binaryRangeSearch(inputArray, condition);
+    expect(result).toStrictEqual(expectedResult);
+  });
+
+  test('Finds element when it is last in the array', () => {
+    const condition = n => n >= 26;
+
+    // Expected result
+    const expectedResult = 26;
+
+    const result = binaryRangeSearch(inputArray, condition);
+    expect(result).toStrictEqual(expectedResult);
+  });
+
+  test('Return undefined if no elements has been found', () => {
+    const condition = n => n >= 27;
+
+    // Expected result
+    const expectedResult = undefined;
+
+    const result = binaryRangeSearch(inputArray, condition);
+    expect(result).toStrictEqual(expectedResult);
+  });
+
+  test('Return undefined given empty array', () => {
+    const condition = n => n >= 27;
+
+    // Expected result
+    const expectedResult = undefined;
+
+    const result = binaryRangeSearch([], condition);
+    expect(result).toStrictEqual(expectedResult);
+  });
+
+  test('Return correct result for an array with single item', () => {
+    const condition = n => n >= 27;
+
+    // Expected result
+    const expectedResult = 27;
+
+    const result = binaryRangeSearch([27], condition);
+    expect(result).toStrictEqual(expectedResult);
+  });
+});
