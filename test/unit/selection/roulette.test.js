9d33d9ca09113a55e8fb81f4e17fe7d28b1a10c3
lukix <lukix.95@gmail.com>
2018-11-09 21:00:32 +0100

    Add more test cases for roulette selection

--- test/unit/selection/roulette.test.js
+++ test/unit/selection/roulette.test.js
@@ -57,4 +57,69 @@ describe('roulette', () => {
     const result = GMO.selection.roulette({ minimizeFitness: true })(evaluatedPopulation, random);
     expect(result).toStrictEqual(offspring);
   });
+
+  test('Returns correct parents population for uniform fitnesses', () => {
+    // Input
+    const evaluatedPopulationWithUniformFitnesses = [
+      { fitness: 2, individual: 'A' },
+      { fitness: 2, individual: 'B' },
+      { fitness: 2, individual: 'C' },
+      { fitness: 2, individual: 'D' },
+      { fitness: 2, individual: 'E' },
+    ];
+    const random = cyclicProvider([
+      0.0,
+      0.99,
+      0.5,
+      0.7,
+      0.98,
+    ]);
+
+    // Expected result
+    const offspring = [
+      { fitness: 2, individual: 'A' },
+      { fitness: 2, individual: 'E' },
+      { fitness: 2, individual: 'C' },
+      { fitness: 2, individual: 'D' },
+      { fitness: 2, individual: 'E' },
+    ];
+
+    const roulette = GMO.selection.roulette({ minimizeFitness: true });
+    const result = roulette(evaluatedPopulationWithUniformFitnesses, random);
+    expect(result).toStrictEqual(offspring);
+  });
+
+  test('Returns correct parents population for negative fitnesses', () => {
+    // Input
+    const evaluatedPopulationWithUniformFitnesses = [
+      { fitness: -3, individual: 'A' },
+      { fitness: 2, individual: 'B' },
+      { fitness: 2, individual: 'C' },
+      { fitness: 2, individual: 'D' },
+      { fitness: 2, individual: 'E' },
+      { fitness: 2, individual: 'F' },
+    ];
+    const random = cyclicProvider([
+      0.0,
+      0.99,
+      0.5,
+      0.7,
+      0.98,
+      0.5,
+    ]);
+
+    // Expected result
+    const offspring = [
+      { fitness: 2, individual: 'B' },
+      { fitness: 2, individual: 'F' },
+      { fitness: 2, individual: 'D' },
+      { fitness: 2, individual: 'E' },
+      { fitness: 2, individual: 'F' },
+      { fitness: 2, individual: 'D' },
+    ];
+
+    const roulette = GMO.selection.roulette({ minimizeFitness: false });
+    const result = roulette(evaluatedPopulationWithUniformFitnesses, random);
+    expect(result).toStrictEqual(offspring);
+  });
 });
