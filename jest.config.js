ea1b8461fadd5341594f6fc7bf167b5c24a0f77d
lukix <lukix.95@gmail.com>
2018-11-17 21:10:22 +0100

    Add tests for elitism succession

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ module.exports = {
   coveragePathIgnorePatterns: ['./test/test-utils/'],
   coverageThreshold: {
     global: {
-      branches: 72,
-      functions: 90,
-      lines: 90,
-      statements: 90,
+      statements: 94,
+      branches: 75,
+      functions: 94,
+      lines: 94,
     },
   },
 };
