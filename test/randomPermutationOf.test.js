3691367555601fb094a6f94bcd179c5e20860c17
lukix <lukix.95@gmail.com>
2018-11-17 21:33:43 +0100

    Add tests for randomPermutationOf

---
+++ test/randomPermutationOf.test.js
@@ -0,0 +1,20 @@
+const rand = require('random-seed');
+const Genemo = require('../lib');
+
+describe('Find Sequence', () => {
+  test('Sequence matches snapshot', () => {
+    const { random } = rand.create('seed');
+    const valuesSet = [-1, 0, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    const result = Genemo.randomPermutationOf(valuesSet)(random);
+    expect(result).toMatchSnapshot('randomPermutationOf');
+  });
+
+  test('works for empty valuesSet', () => {
+    const { random } = rand.create('seed');
+    const valuesSet = [];
+
+    const result = Genemo.randomPermutationOf(valuesSet)(random);
+    expect(result).toStrictEqual([]);
+  });
+});
