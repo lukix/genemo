f81a56b5e7d93a308f5b3d46e4937b2215f3187e
lukix <lukix.95@gmail.com>
2019-06-20 15:37:19 +0200

    Add checkProps function

--- src/utils/typeChecking.js
+++ src/utils/typeChecking.js
@@ -16,6 +16,33 @@ const withPropsChecking = R.curry((funcName, func, propTypes) => (props) => {
   return func(props);
 });
 
+const checkProps = ({ functionName, propTypes, props }) => {
+  if (typeof props !== 'object') {
+    throw Error(`Missing argument for ${functionName}`);
+  }
+
+  Object.entries(propTypes).forEach(([key, { type, isRequired }]) => {
+    const actualType = typeof props[key];
+    if (actualType === 'undefined') {
+      if (isRequired) {
+        throw Error(`Property \`${key}\` is required in \`${functionName}\`, but received undefined`);
+      }
+    } else if (actualType !== type) {
+      throw Error(`Invalid Property \`${key}\` supplied to \`${functionName}\`, expected ${type}, but received ${actualType}`);
+    }
+  });
+};
+
+const types = {
+  FUNCTION: 'function',
+  NUMBER: 'number',
+  STRING: 'string',
+  BOOLEAN: 'boolean',
+  OBJECT: 'object',
+};
+
 module.exports = {
   withPropsChecking,
+  checkProps,
+  types,
 };
