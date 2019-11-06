359288fa32a8b039954f6b472e6368bff97422d2
lukix <lukix.95@gmail.com>
2019-11-06 19:18:08 +0100

    Fix Timer import

--- test/unit/utils/timer.test.ts
+++ test/unit/utils/timer.test.ts
@@ -1,4 +1,4 @@
-import Timer from '../../../src/utils/Timer';
+import Timer from '../../../src/utils/timer';
 
 describe('Timer', () => {
   test('To throw an error when "stop" is called without calling "start" beforehand', () => {
