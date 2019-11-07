08387748d026777360b28d5d58ee847de5d69216
lukix <lukix.95@gmail.com>
2019-11-07 21:24:26 +0100

    Add types key to package.json

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## 1.3.1 - Not Released
+- Migrated codebase to TypeScript.
+
 ## 1.3.0 - 2019-09-12
 - Added `Genemo.reproduceBatch` function to make implementation of multithreading easier.
 - Added an example of implementing multithreading for genetic algorithm.
--- src/generateInitialPopulation.ts
+++ src/generateInitialPopulation.ts
@@ -1,11 +1,22 @@
 import { checkProps, types } from './utils/typeChecking';
+import {
+  Rng,
+  Population,
+} from './sharedTypes';
 
 const propTypes = {
   generateIndividual: { type: types.FUNCTION, isRequired: true },
   size: { type: types.NUMBER, isRequired: true },
 };
 
-export const generateInitialPopulation = (options) => {
+interface GenerateInitialPopulationOptions<Individual> {
+  generateIndividual: (random: Rng) => Individual;
+  size: number;
+}
+
+export const generateInitialPopulation = <Individual>(
+  options: GenerateInitialPopulationOptions<Individual>,
+): (random: Rng) => Population<Individual> => {
   checkProps({
     functionName: 'Genemo.generateInitialPopulation',
     props: options,
